db45d383cb6ed6c252321fa9abda4c46287e44a1
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-10 20:04:46 +0200

    Access routes in code

--- src/components/User.vue
+++ src/components/User.vue
@@ -4,7 +4,17 @@
       User
     </div>
     <div class="card-body">
-      ...
+      <button type="button" class="btn btn-danger" @click="goHome">Danger</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+}
+</script>
