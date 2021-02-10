f5adb43ba137ac7a4c2ef0aaa585319ae9062c77
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-10 19:44:17 +0200

    Bootstap configured

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
 import Home from "./components/Home";
@@ -8,6 +13,9 @@ import User from "./components/User";
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 const router = new VueRouter({
   routes: [
